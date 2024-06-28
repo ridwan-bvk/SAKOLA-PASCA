a656e59f8d47290be9de5a6e3c7db0c756138ca1
ridwan-bvk <readsmath@gmail.com>
2024-06-28 17:30:57 +0700

    tambahdata

---
+++ SEMESTER 1/DATA MINING/_TUGAS/UAS/PERTEMUAN - 14 BERTTOPIC/bertopic.py
@@ -0,0 +1,19 @@
+# import pandas as pd 
+# import numpy as np
+# from bertopic import BERTopic
+
+# df = pd.read_csv("Indonesian_", engine='python')
+
+# print(df.head)
+
+from bertopic import BERTopic
+from sklearn.datasets import fetch_20newsgroups
+
+docs = fetch_20newsgroups(subset='all',  remove=('headers', 'footers', 'quotes'))["data"]
+
+seed_topic_list = [["drug", "cancer", "drugs", "doctor"],
+                   ["windows", "drive", "dos", "file"],
+                   ["space", "launch", "orbit", "lunar"]]
+
+topic_model = BERTopic(seed_topic_list=seed_topic_list)
+topics, probs = topic_model.fit_transform(docs)
